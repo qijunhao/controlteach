59caeb5d92f311053ae290cf3ce17e1a3f6406d9
qijunhao <2020302191427@whu.edu.cn>
2023-08-09 08:00:38 +0800

    commit

--- src/main.c
+++ src/main.c
@@ -1,22 +1,3 @@
 ﻿#include "../inc/main.h"
 
-#include <stdio.h>
-
-int main()
-{
-	Queue* queue = InitQueue(8);
-
-	EnQueue(queue, 10);
-	EnQueue(queue, 9);
-	EnQueue(queue, 8);
-	EnQueue(queue, 7);
-
-	float rec;
-	DeQueue(queue, &rec);
-	printf("%.2f\n", rec);
-	DeQueue(queue, &rec);
-	printf("%.2f\n", rec);
-	DeQueue(queue, &rec);
-	printf("%.2f", rec);
-	return 0;
-}
+int main() { return 0; }
